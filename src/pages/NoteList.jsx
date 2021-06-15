15ca4bbdf2c5531e41b66bf4629dd63fff9aab34
Hugo Mouto <hmoutol@gmail.com>
2021-06-15 15:58:18 -0300

    feat: Filter input working for searching note

--- src/pages/NoteList.jsx
+++ src/pages/NoteList.jsx
@@ -9,7 +9,9 @@ export default class NoteList extends Component {
     this.state = {
       notes: [],
       loading: true,
+      searchText: '',
     }
+    this.handleSearch = this.handleSearch.bind(this)
   }
     
   componentDidMount() {
@@ -18,21 +20,30 @@ export default class NoteList extends Component {
 
   async fetchNotes() {
     const notes = await CRUD.getNotes();
-    this.setState({ notes, loading: false });
+    this.setState({ notes, loading: false});
+  }
+
+  handleSearch({ target }) {
+    const { value } = target
+    this.setState({
+      searchText: value,
+    })
   }
 
 
-
   render() {
-    const { loading, notes } = this.state
+    const { loading, notes, searchText } = this.state
     if (!notes || notes.length === 0) {
       return `Ainda não há notas`
     }
     return (
       <div>
+        <label htmlFor="">Busca
+          <input onChange={this.handleSearch}  value={searchText} type="text" />
+        </label>
         { loading
           ? <Loading />
-          : notes.map(note => <NoteCard key={note.id} noteData={note}/>)
+          : notes.filter((note) => note.noteContent.includes(searchText)).map(note => <NoteCard key={note.id} noteData={note}/>)
         }
       </div>
     )
